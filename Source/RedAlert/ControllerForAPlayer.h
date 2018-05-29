d2c0b48ec63128b53875c04dd2aad76e8cdb96fb
Torcx <1627301643@qq.com>
2018-05-29 22:16:34 +0800

    CameraPawn file in c++ folder is set as our default pawn, ControllerForAPlayer file in c++ folder is set as our default playercontroller.

---
+++ Source/RedAlert/ControllerForAPlayer.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "ControllerForAPlayer.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class REDALERT_API AControllerForAPlayer : public APlayerController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
