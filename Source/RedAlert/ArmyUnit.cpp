a02b32a7d62fbd178fcd21ae7e70100ad4267030
acdzh <1069436872@qq.com>
2018-06-02 17:20:38 +0800

    gg

--- Source/RedAlert/ArmyUnit.cpp
+++ Source/RedAlert/ArmyUnit.cpp
@@ -1,7 +1,19 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "ArmyUnit.h"
-#define AttackState 0
+
+#include "UObject/ConstructorHelpers.h"
+#include "Camera/CameraComponent.h"
+#include "Components/DecalComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/PlayerController.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "HeadMountedDisplayFunctionLibrary.h"
+#include "Materials/Material.h"
+#include "Engine/World.h"
+
+#define AttackState 1
 //might define or enum
 
 
@@ -9,6 +21,7 @@ AArmyUnit::AArmyUnit()
 {
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
 }
 
 // Called when the game starts or when spawned
@@ -28,10 +41,21 @@ void AArmyUnit::Tick(float DeltaTime)
 }
 
 
-void AArmyUnit::MoveTo(float x, float y)
+void AArmyUnit::MoveTo(FVector VecTarget)
 {
+	//to change the actor's Rotation (to let it see the target
+	FVector VecNowLocation = GetActorLocation();
+	FVector VecDirection = VecTarget - VecNowLocation;
+	VecDirection.Normalize();
+	FRotator R = VecDirection.Rotation();
+	SetActorRotation(R);
 
+	SetActorLocation(VecTarget);
+	
 }
+
+
+
 
 void AArmyUnit::Attack(ARedAlertObjectBase *Target)
 {
