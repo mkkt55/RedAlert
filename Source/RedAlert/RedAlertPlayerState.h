f03a7418e773c9f7df9c4a26d66717e1d61f7f9e
Torcx <1627301643@qq.com>
2018-06-15 21:26:08 +0800

    update

---
+++ Source/RedAlert/RedAlertPlayerState.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "RTSPlayerState.h"
+#include "RedAlertPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class REDALERT_API ARedAlertPlayerState : public ARTSPlayerState
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
