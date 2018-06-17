4288544808898fc5c55da58fc3bd55b3d1d79d23
Torcx <1627301643@qq.com>
2018-06-17 23:05:50 +0800

    find potential fatal errors and deal with it

---
+++ Source/RedAlert/RedAlertGameState.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "RTSGameState.h"
+#include "RedAlertGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class REDALERT_API ARedAlertGameState : public ARTSGameState
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
