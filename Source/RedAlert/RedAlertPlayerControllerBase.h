74718e133bfcb57f4b0f44771bfff37b082bfc4d
Torcx <1627301643@qq.com>
2018-06-22 21:52:04 +0800

    submit before trying to modify resource code of plugin

---
+++ Source/RedAlert/RedAlertPlayerControllerBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "RTSPlayerController.h"
+#include "RedAlertPlayerControllerBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class REDALERT_API ARedAlertPlayerControllerBase : public ARTSPlayerController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
